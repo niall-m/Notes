7d3022c7ec0621862047c286b3b2d9f8ced582b8
Niall Mahford <niall.mahford@gmail.com>
2018-08-22 13:59:31 -0800

    Two Number Sum O(n log(n)) Python.

--- algorithms/Python/twoNumberSum.py
+++ algorithms/Python/twoNumberSum.py
@@ -19,3 +19,18 @@ def twoNumberSum2(array, targetSum):
             nums[num] = True
     return []
 # e.g. twoNumberSum([1, 4, 2, 6, 9], 11)
+
+# O(nlog(n)) time | O(1) space
+def twoNumberSum3(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
