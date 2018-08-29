ee638c3b436961e04104215c63930e83d25e8a4f
Niall Mahford <niall.mahford@gmail.com>
2018-08-28 17:59:15 -0800

    BST Construction Remove Python.

--- algorithms/Python/bstConstruction.py
+++ algorithms/Python/bstConstruction.py
@@ -35,3 +35,45 @@ class BST:
             else:
                 return True
         return False
+
+    # Average: O(Log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) sace
+    def remove(self, value, parentNode = None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else: # we found the target
+                if currentNode.left is not None and currentNode.right is not None:
+                    # target has 2 children
+                    currentNode.value = currentNode.right.findMinValue()
+                    # currentNode.value = smallest value of right subtree
+                    currentNode.right.remove(currentNode.value, currentNode)
+                # root node case, has no parent node
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else: #remove root with no parent or children
+                        currentNode.value = None
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+                break
+        return self
+
+    def findMinValue(self):
+        currentNode = self
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
