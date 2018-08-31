4b2536eb0c8ec7c8677c3730a3522f1edf72b2c4
Niall Mahford <niall.mahford@gmail.com>
2018-08-31 14:01:56 -0800

    Nth Fibonacci Recursive O(n).

--- algorithms/Javascript/nthFibonacci.js
+++ algorithms/Javascript/nthFibonacci.js
@@ -9,3 +9,14 @@ function nthFib(n){
     return nthFib(n - 1) + nthFib(n - 2);
   }
 }
+
+// Recursive with Memoization Hash
+// O(n) Time | O(n) Space
+function nthFib2(n, memo = {1: 0, 2: 1}) { // initialize memo-hash with 2 base cases above
+  if (n in memo) { // see whether this fib has already been calculated
+    return memo[n];
+  } else {
+    memo[n] = nthFib2(n - 1, memo) + nthFib2(n - 2, memo); // store fib in hash to circumvent redundant calculations
+    return memo[n];
+  }
+}
