1b0a7a09ac7345999151d0fb25489bad0b8c0329
Niall Mahford <niall.mahford@gmail.com>
2018-09-03 19:27:07 -0800

    Nth Fibonacci Py Recursive O(n).

--- algorithms/Python/nthFibonacci.py
+++ algorithms/Python/nthFibonacci.py
@@ -7,4 +7,12 @@ def getNthFib(n):
         return 0
     else:
         return getNthFib(n - 1) + getNthFib(n - 2)
-        
+
+# Recursive with Memoization Hash
+# O(n) Time | O(n) Space
+def getNthFib2(n, memoize = {1: 0, 2: 1}):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = getNthFib2(n - 1, memoize) + getNthFib2(n - 2, memoize)
+        return memoize[n]
