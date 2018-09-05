e92442cc0b7ef4cd73a020e8dc99f2146cbff34f
Niall Mahford <niall.mahford@gmail.com>
2018-09-04 18:31:01 -0800

    Nth Fibonacci Py Iterative

--- algorithms/Python/nthFibonacci.py
+++ algorithms/Python/nthFibonacci.py
@@ -16,3 +16,15 @@ def getNthFib2(n, memoize = {1: 0, 2: 1}):
     else:
         memoize[n] = getNthFib2(n - 1, memoize) + getNthFib2(n - 2, memoize)
         return memoize[n]
+
+# Iterative solution
+# O(n) Time | O(1) Space
+def getNthFib3(n):
+    lastTwo = [0, 1]
+    counter = 3
+    while counter <= n:
+        nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    return lastTwo[1] if n > 1 else lastTwo[0]
