282be0fd77e6e029c809447e7af1bb0056519a31
Niall Mahford <niall.mahford@gmail.com>
2018-09-11 18:34:52 -0800

    Iterative Binary Search Py.

--- algorithms/Python/binarySearch.py
+++ algorithms/Python/binarySearch.py
@@ -14,3 +14,19 @@ def binarySearchHelper(array, target, left, right):
         return binarySearchHelper(array, target, left, middle - 1)
     else: # target > potentialMatch, search right half of array
         return binarySearchHelper(array, target, middle + 1, right)
+
+# Iterative Binary Search
+# O(Log(n)) Time | O(1) Space
+def binarySearch2(array, target):
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        potentialMatch = array[middle]
+        if target == potentialMatch:
+            return middle
+        elif target < potentialMatch:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
