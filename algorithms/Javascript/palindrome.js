1c32d6089f706e59baf800a980de7dc4d45a67fc
Niall Mahford <niall.mahford@gmail.com>
2018-09-24 18:17:46 -0800

    Palindrome Iterative.

--- algorithms/Javascript/palindrome.js
+++ algorithms/Javascript/palindrome.js
@@ -23,3 +23,15 @@ function isPalindrome3(string, i = 0) {
   return i >= j ? true : string[i] === string[j] && isPalindrome3(string, i + 1);
   // do the first and last letters match, and is everything in between a palindrome as well?
 }
+
+// O(n) Time | O(1) Space
+function isPalindrome4(string) {
+  let startIdx = 0;
+  let endIdx = string.length - 1;
+  while (startIdx < endIdx) {
+    if (string[startIdx] !== string[endIdx]) return false;
+    startIdx++;
+    endIdx--;
+  }
+  return true;
+}
