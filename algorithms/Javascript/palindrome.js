45702918c5482772152ef490f6df37313080fd80
Niall Mahford <niall.mahford@gmail.com>
2018-09-24 17:12:38 -0800

    Palindrome O(n).

--- algorithms/Javascript/palindrome.js
+++ algorithms/Javascript/palindrome.js
@@ -6,3 +6,12 @@ function isPalindrome(string) {
   }
   return string === reversed;
 }
+
+// O(n) Time | O(n) Space
+function isPalindrome2(string) {
+  const reversed = [];
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed.push(string[i]);
+  }
+  return string === reversed.join("");
+}
