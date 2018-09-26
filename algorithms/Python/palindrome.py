6ee6c08442df4835a9737b497c1ab0e4a3d60179
Niall Mahford <niall.mahford@gmail.com>
2018-09-25 22:05:26 -0800

    Typo.

--- algorithms/Python/palindrome.py
+++ algorithms/Python/palindrome.py
@@ -1,6 +1,6 @@
 # O(n^2) Time | O(n) Space
-def isPalindrome(str):
+def isPalindrome(string):
     reversedStr = ""
-    for i in reversed(range(len(str))):
-        reversedStr += str[i]
-    return str == reversedStr
+    for i in reversed(range(len(string))):
+        reversedStr += string[i]
+    return string == reversedStr
