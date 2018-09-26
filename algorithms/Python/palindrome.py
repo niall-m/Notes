d295d0ee6316b4a3559b19d7da56b1fb92b179fe
Niall Mahford <niall.mahford@gmail.com>
2018-09-25 22:15:41 -0800

    Palindrome Py O(n).

--- algorithms/Python/palindrome.py
+++ algorithms/Python/palindrome.py
@@ -4,3 +4,11 @@ def isPalindrome(string):
     for i in reversed(range(len(string))):
         reversedStr += string[i]
     return string == reversedStr
+
+# O(n) Time | O(n) Space
+def isPalindrome2(string):
+    reversedChars = []
+    for i in reversed(range(len(string))):
+        reversedChars.append(string[i])
+    return string == "".join(reversedChars)
+ 
