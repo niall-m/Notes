95b691aab35bc95a9045f57f3a2b26605dd1fd5a
Niall Mahford <niall.mahford@gmail.com>
2018-09-25 22:41:44 -0800

    Palindrome Py Iterative.

--- algorithms/Python/palindrome.py
+++ algorithms/Python/palindrome.py
@@ -17,3 +17,15 @@ def isPalindrome2(string):
 def isPalindrome3(string, i = 0):
     j = len(string) - 1 - i
     return True if i >= j else string[i] == string[j] and isPalindrome3(string, i + 1)
+
+# O(n) Time | O(1) Space
+# Iterative
+def isPalindrome4(string):
+    startIdx = 0
+    endIdx = len(string) - 1
+    while startIdx < endIdx:
+        if string[startIdx] != string[endIdx]:
+            return False
+        startIdx += 1
+        endIdx -= 1
+    return True
