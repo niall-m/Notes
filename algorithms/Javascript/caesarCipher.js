72d613122ad7306b051a392629e07d90c5995b36
Niall Mahford <niall.mahford@gmail.com>
2018-09-27 22:59:49 -0800

    caesarCipher JS O(n).

--- algorithms/Javascript/caesarCipher.js
+++ algorithms/Javascript/caesarCipher.js
@@ -1,4 +1,5 @@
 // O(n) Time | O(n) Space
+// Using UTF-16 code units
 function caesarCipher(string, key) {
   const cipheredChars = [];
   const newKey = key % 26;
@@ -12,4 +13,17 @@ function findNewLetter(letter, key) {
   const charIdx = letter.charCodeAt() - "a".charCodeAt();
   const newCharIdx = (charIdx + key) % 26;
   return String.fromCharCode(newCharIdx + "a".charCodeAt());
+}
+
+// O(n) Time | O(n) Space
+// Using Alphabet
+function caesarCipher2(string, key) {
+  const cipheredChars = [];
+  const newKey = key % 26;
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  for (const letter of string) {
+    const newLetter = (alphabet.indexOf(letter) + key) % alphabet.length;
+    cipheredChars.push(alphabet[newLetter]);
+  }
+  return cipheredChars.join("");
 }
