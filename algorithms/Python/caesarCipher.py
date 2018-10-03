4504468f29c8e857bbe41685ec0d631041e9fa26
Niall Mahford <niall.mahford@gmail.com>
2018-10-02 16:12:50 -0800

    caesarCipher Py.

--- algorithms/Python/caesarCipher.py
+++ algorithms/Python/caesarCipher.py
@@ -6,3 +6,14 @@ def caesarCipher(string, key):
         newLetter = (alphabet.index(letter) + key) % len(alphabet)
         cipheredChars.append(alphabet[newLetter])
     return "".join(cipheredChars)
+
+# O(n) Time | O(n) Space
+def caesarCipher2(string, key):
+    cipheredChars = []
+    newKey = key % 26
+    for letter in string:
+        charIdx = ord(letter) - ord("a")
+        newCharIdx = (charIdx + key) % 26
+        newChar = chr(ord("a") + newCharIdx)
+        cipheredChars.append(newChar)
+    return "".join(cipheredChars)
