f695df574e6e18cca48b4092caf0ab3dd76359ae
Niall Mahford <niall.mahford@gmail.com>
2018-10-07 22:11:48 -0800

    Recursive Binary Tree Inversion.

--- algorithms/Javascript/invertBinaryTree.js
+++ algorithms/Javascript/invertBinaryTree.js
@@ -12,3 +12,18 @@ function invertBinaryTree(tree) {
     queue.push(currentNode.right);
   }
 }
+
+// O(n) Time | O(d) Space
+// Recursive
+function invertBinaryTree2(tree) {
+  if (tree === null) return;
+  swapLeaves(tree);
+  invertBinaryTree2(tree.left);
+  invertBinaryTree2(tree.right);
+}
+
+function swapLeaves(currentNode) {
+  const left = currentNode.left;
+  currentNode.left = currentNode.right;
+  currentNode.right = left;
+}
