6cf60be82b9c7ca3e02d89a1a00ec0783c2401f4
Niall Mahford <niall.mahford@gmail.com>
2018-10-25 11:44:12 -0800

    Linked List remove and update pointers.

--- algorithms/Javascript/linkedListConstruction.js
+++ algorithms/Javascript/linkedListConstruction.js
@@ -29,12 +29,21 @@ class DoubleLinkedList {
 	}
 	
   remove(node) {
-
-	}
+		if (node === this.head) this.head = this.head.next;
+		if (node === this.tail) this.tail = this.tail.prev;
+		this.updateNodePointers(node);
+  }
 	
   containsNodeWithValue(value) {
     let node = this.head;
 	  while (node !== null && node.value !== value) node = node.next;
 	  return node !== null;
-  }
+	}
+	
+	updateNodePointers(node) {
+		if (node.prev !== null) node.prev.next = node.next;
+		if (node.next !== null) node.next.prev = node.prev;
+		node.prev = null;
+		node.next = null;
+	}
 }
