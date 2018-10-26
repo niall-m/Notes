80d914fbcdfdd54bb9ff1cc184f98421324a2614
Niall Mahford <niall.mahford@gmail.com>
2018-10-26 13:37:51 -0800

    Linked List Construction remove and update pointers.

--- algorithms/Python/linkedListConstruction.py
+++ algorithms/Python/linkedListConstruction.py
@@ -22,10 +22,23 @@ class DoublyLinkedList:
         pass
 
     def remove(self, node):
-        pass
+        if node == self.head: # are we dealing with head or tail?
+            self.head = self.head.next
+        if node == self.tail: # Null <== 1 ==> Null, must update next and prev
+            self.tail = self.tail.prev
+        self.updateNodePointers(node)
 
     def containsNodeWithValue(self, value):
         node = self.head
         while node is not None and node.value != value: # not an empty list, haven't passed tail
             node = node.next
         return node is not None
+
+    def updateNodePointers(self, node):
+        # update pointers of surrounding nodes, then remove pointers of given node
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.prev = None
+        node.next = None
